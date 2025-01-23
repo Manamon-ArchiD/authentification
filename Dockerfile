84442e718d151346320c1fcda92c277556fd9225
Louis Bruneteau <bruneteau.louis@gmail.com>
2025-01-23 11:04:43 +0100

    c'est un peu mieux mais ça connecte po

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+
+FROM node:latest
+
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json
+COPY package*.json ./
+
+# Install dependencies
+RUN
--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://admin:password@mongodb:27017/manamon-auth?authSource=admin&directConnection=true");
+await mongoose.connect("mongodb://admin:password@localhost:27017/auth_mongo?authSource=admin");
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
